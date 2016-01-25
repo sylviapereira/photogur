2b5da5341a05495df15fd042ecc408cc4aa6ada6
Your Name <sylviapereira@gmail.com>
2016-01-25 17:21:41 -0500

    Making each picture show at its own URL with dynamic routes

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :pictures
-  
-  get '/pictures' => 'pictures#index'
+  get 'pictures' => 'pictures#index'
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +56,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
