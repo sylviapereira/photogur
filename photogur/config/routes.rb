a205a07cd8b61443360c53ee2394f63569adcdf6
Your Name <sylviapereira@gmail.com>
2016-01-25 17:40:59 -0500

    Refactoring views to use helpers and adding navigation

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
-  .
 
   post 'pictures' => 'pictures#create' # this is a new line of code
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
