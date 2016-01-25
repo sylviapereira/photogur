e117d21d11a907a89004370e169c69c8a1c325b7
Your Name <sylviapereira@gmail.com>
2016-01-25 16:41:23 -0500

    Creating Picture model in database

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pictures' => 'pictures#index'
+  resources :pictures
+  
+  get '/pictures' => 'pictures#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
