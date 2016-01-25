3d3bc9a35d4ff488ccd8dfecd65c30b5c4010976
Your Name <sylviapereira@gmail.com>
2016-01-25 17:38:16 -0500

    Configuring root as pictures#index

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
+  .
 
   post 'pictures' => 'pictures#create' # this is a new line of code
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
